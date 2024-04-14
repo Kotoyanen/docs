215f5f0a5dd95da3040e62b38f5fce95aeb0bd66
Yeo Wen Qin, Kaynn <wqyeo@outlook.com>
2024-04-14 23:23:01 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kotoyanen.github.io',
+	base: 'docs',
 	integrations: [
 		starlight({
 			title: 'Docs with Tailwind',
